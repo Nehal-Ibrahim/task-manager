5d12c4fa8fe6eb3ddf369d3ec6b73402ed9c425d
Nehal-Ibrahim <nehalibrahim026@gmail.com>
2021-12-02 23:07:41 +0200

    fiiiii

--- src/app/views/edit-task/edit-task.component.ts
+++ src/app/views/edit-task/edit-task.component.ts
@@ -11,7 +11,7 @@ import { TaskService } from 'src/app/services/task.service';
 export class EditTaskComponent implements OnInit {
 
   constructor(private taskservice:TaskService , private route:ActivatedRoute) { }
-  task:Tasks[]=[]
+  task:Tasks={}
 updateTask(task:Tasks){
     this.taskservice.updateTask(task._id,task).subscribe({
       next:()=>{
